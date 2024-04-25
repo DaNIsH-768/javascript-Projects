bd811a1ef035f1e19fca6933ef61895d9325f7fc
Danish Raina <danishraina768@gmail.com>
2024-04-25 22:43:12 +0530

    Added the search functionality and the dropdown menu

--- Movie-Fight/index.js
+++ Movie-Fight/index.js
@@ -10,12 +10,56 @@ const fetchData = async (movieName) => {
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
-const onInput = (e) => {
-  fetchData(e.target.value);
-};
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu" >
+    <div class="dropdown-content results"></div>
+  </div>
+</div>
+`;
 
 const inputBox = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultBox = document.querySelector(".results");
+
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
+  resultBox.innerHTML = ``;
+
+  dropdown.classList.add("is-active");
+
+  for (let movie of movies) {
+    const movieTag = document.createElement("a");
+    movieTag.classList.add("dropdown-item");
+
+    movieTag.innerHTML = `
+    <img src="${movie.Poster}" />
+    <p>${movie.Title}</p>
+    `;
+    resultBox.appendChild(movieTag);
+  }
+};
+
 inputBox.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (e) => {
+  if (!root.contains(e.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
